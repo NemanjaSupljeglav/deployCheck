d07b715cfbdd59d2b039646ec68f1bafcffe2522
N3manjaS <nemanjaaasssupljeglav@gmail.com>
2023-07-14 14:44:58 +0200

    add js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a29f9e625807f09ec90a";
+/******/ 	var hotCurrentHash = "212b62e3e0131b97c3c4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
